60c1713ffdd8f553be9cb39875b97a1ffe2f6942
SANGEETH-KUMAR07 <kumarsangeeth842@gmail.com>
2025-08-30 22:46:20 +0530

    Create computer.sv

---
+++ computer.sv
@@ -0,0 +1,20 @@
+module computer #(
+   parameter WIDTH=32
+ ) (
+   clk,reset,inst,pc_out,aluout,memout
+ );
+ input clk,reset;
+ output [WIDTH-1:0] pc_out,inst,memout;
+ inout [WIDTH-1:0] aluout;
+ wire [WIDTH-1:0] datao,memo;
+ wire wmem ;
+ cpu_datapath main(inst,memo,reset,clk,
+   pc_out,aluout,datao, wmem);
+ instruction_memory ins(pc_out,inst);
+ datamemo data(aluout,datao,wmem,clk,
+   memout);
+   //clk,memout,data,aluout,wmem);
+   // (clk,dataout,datain,addr,we);
+   // address,datain,we,clk,
+   //dataout
+ endmodule
